bbe736fabf656b82533b6f07130729e0c3b6a7a5
Phạm Trọng Tính <47069678+PTTinh@users.noreply.github.com>
2026-01-25 01:14:34 +0700

    Remove course and room routes
    Removed course and room related routes from web.php.

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,6 @@ Route::get('/lien-he', [HomeController::class, 'contacts'])->name('contacts');
 Route::get('/tin-tuc', [NewsController::class, 'index'])->name('news.index');
 Route::get('/tin-tuc/danh-muc/{slug}', [NewsController::class, 'category'])->name('news.category');
 Route::get('/tin-tuc/{slug}', [NewsController::class, 'show'])->name('news.show');
-Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
-Route::get('/courses/{slug}', [CourseController::class, 'show'])->name('courses.show');
-Route::post('/courses/registration/', [CourseController::class, 'course_registration'])->name('courses.registration');
-Route::get('/rooms', [RoomController::class, 'index'])->name('rooms.index');
-Route::get('/rooms/{id}', [RoomController::class, 'show'])->name('rooms.show');
-Route::post('/rooms/bookings', [RoomController::class, 'roomBookings'])->name('rooms.bookings');
-Route::get('/contacts', [HomeController::class, 'contacts'])->name('contacts');
-Route::get('/news', [App\Http\Controllers\NewsController::class, 'index'])->name('news.index');
-Route::get('/news/category/{slug}', [App\Http\Controllers\NewsController::class, 'category'])->name('news.category');
-Route::get('/news/{slug}', [App\Http\Controllers\NewsController::class, 'show'])->name('news.show');
 Route::get('/search', [HomeController::class, 'search'])->name('search');
 
 // Admin routes for room booking details 
