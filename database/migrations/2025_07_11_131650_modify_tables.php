3d59bd1287e2158c7b3f0757fe8da04fced70162
Phạm Trọng Tính <47069678+PTTinh@users.noreply.github.com>
2026-01-25 01:30:27 +0700

    Update 2025_07_11_131650_modify_tables.php

--- database/migrations/2025_07_11_131650_modify_tables.php
+++ database/migrations/2025_07_11_131650_modify_tables.php
@@ -38,10 +38,6 @@ return new class extends Migration
             // thêm cột created_by
             $table->unsignedBigInteger('created_by')->nullable()->after('id');
             $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
-            // thêm cột đánh dấu trang thiết bị là miễn phí hay không
-            $table->boolean('is_free')->default(false)->after('price');
-            // sửa cột price để cho phép giá trị null, 15 chữ số, 2 chữ số thập phân
-            $table->decimal('price', 15, 2)->nullable()->change();
         });
     }
 
@@ -66,8 +62,6 @@ return new class extends Migration
         Schema::table('equipments', function (Blueprint $table) {
             $table->dropForeign(['created_by']);
             $table->dropColumn('created_by');
-            $table->dropColumn('is_free');
-            $table->decimal('price', 15, 2)->change();
         });
     }
 };
